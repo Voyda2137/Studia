865089b4e60f3182cc0456d8707ac85d8570c3d9
Voyda2137 <wojdagracjan@gmail.com>
2022-11-18 17:41:01 +0100

    cv2 improvement

--- przetwarzanie_obrazow/LAB1/zad1.py
+++ przetwarzanie_obrazow/LAB1/zad1.py
@@ -1,6 +1,8 @@
 import cv2 as cv
-cv.samples.addSamplesDataSearchPath("C:\\Users\\wojda\\OneDrive\\Dokumenty\\GitHub\\skryptowe_jezyki_LAB\\przetwarzanie_obrazow\\LAB1")
+import os
+path = "C:\\Users\\wojda\\OneDrive\\Dokumenty\\GitHub\\skryptowe_jezyki_LAB\\przetwarzanie_obrazow\\LAB1"
+cv.samples.addSamplesDataSearchPath(path)
 img = cv.imread(cv.samples.findFile('png.png'))
-cv.imwrite('png2.png', img)
+cv.imwrite(os.path.join(path, 'png2.png'), img)
 
 
