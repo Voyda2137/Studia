f8604acbb25d6123db8d5f445633a3497f78b44a
Voyda2137 <wojdagracjan@gmail.com>
2022-11-19 21:45:46 +0100

    Create zad3.py

---
+++ skryptowe_jezyki_programowania/LAB4/zad3.py
@@ -0,0 +1,11 @@
+class drink:
+    def __init__(self, name=str(), price=int(), alc=int(), capacity=int()):
+        self.name = name
+        self.price = price
+        self.alc = alc
+        self.capacity = capacity
+
+vodka = drink('wódka', 8, 40, 50)
+rum = drink('rum', 9, 60, 50)
+cola = drink('cola', 2, 0 ,100)
+ice = drink('lód', 0, 0, 30)
