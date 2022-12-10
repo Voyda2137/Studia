d331483bc1378469ab58828bcae38357dc0cb0e0
Voyda2137 <wojdagracjan@gmail.com>
2022-12-10 13:18:03 +0100

    Update zad3.py

--- skryptowe_jezyki_programowania/LAB4/zad3.py
+++ skryptowe_jezyki_programowania/LAB4/zad3.py
@@ -4,29 +4,45 @@ class drink():
         self.price = price
         self.alc = alc
         self.capacity = capacity
+
     def __add__(self, other):
-        name = self.name + " z " + other.name
-        price = self.price + other.price
-        alc = round(((self.capacity * self.alc) + (other.capacity * other.alc))/(self.capacity + other.capacity))
-        capacity = self.capacity + other.capacity
-        print('Nazwa nowego drinka: ', name, ' Cena: ', price, ' Zawartość alkoholu: ', alc,'% ', 'Jego pojemność: ', capacity)
+        newDrink = drink("", 0, 0, 0)
+        newDrink.name = self.name + " z " + other.name
+        newDrink.price = self.price + other.price
+        newDrink.alc = round(((self.capacity * self.alc) + (other.capacity *
+                    other.alc))/(self.capacity + other.capacity))
+        newDrink.capacity = self.capacity + other.capacity
+        print('Nazwa nowego drinka: ', newDrink.name, ' Cena: ', newDrink.price,
+              ' Zawartość alkoholu: ', newDrink.alc, '% ', 'Jego pojemność: ', newDrink.capacity)
+        return newDrink
+
     def manyDrinks(self, amm):
         price = self.price * amm
         capacity = self.capacity * amm
+
     def __str__(self):
-        print('Nazwa drinka: ', self.name, ' Cena: ', self.price, ' Zawartość alkoholu: ', self.alc,'% ', 'Jego pojemność: ', self.capacity)
+        print('Nazwa drinka: ', self.name, ' Cena: ', self.price,
+              ' Zawartość alkoholu: ', self.alc, '% ', 'Jego pojemność: ', self.capacity)
+
     def __lt__(self, other):
-        return self.alc  < other.alc 
+        if self.price != 0 or other.price != 0:
+            return (self.alc / self.price) < (other.alc / other.price)
+
+
 vodka = drink('wódka', 8, 40, 50)
 rum = drink('rum', 9, 60, 50)
 cola = drink('cola', 2, 0, 100)
 ice = drink('lód', 0, 0, 30)
 wine = drink('wino', 5, 15, 100)
-whiskey = drink('whiskey', 12, 40, 50)        
+whiskey = drink('whiskey', 12, 40, 50)
 
-drink(vodka.__add__(cola))
+drink1 = vodka + rum
+drink2 = vodka + whiskey
+drink3 = whiskey + ice
+drink4 = cola + ice
 drink(vodka.manyDrinks(8))
 drink(cola.__str__())
-arr = [vodka, rum, cola, ice, wine, whiskey]
+arr = [drink1, drink2, drink3, drink4]
 arr.sort()
-
+for drink in arr:
+    print(drink.price)
